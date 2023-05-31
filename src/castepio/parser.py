7fe9ce8a75c9c7f934ab9143096945b294e7ea7d
Scott Thiel <sthiel@umass.edu>
2023-05-31 17:51:33 -0400

    doc parser base

--- src/castepio/parser.py
+++ src/castepio/parser.py
@@ -54,7 +54,7 @@ class Parser(ABC):
         return self.parse()
 
     def end(self, n: int = 0) -> bool:
-        """Checks n tokens ahead to find the EOF token. Default is 0."""
+        """Checks n tokens ahead to find the EOF token. Default is 0, the current token."""
         if n == 0:
             return self.tokens[self.current].token_type == TokenType.EOF
         else:
@@ -68,29 +68,36 @@ class Parser(ABC):
         return self.tokens[self.current + n]
 
     def peek_current(self) -> Token:
+        """Peeks the current token. Identical to calling peek() with no args."""
         return self.peek()
 
     def peek_next(self) -> Token:
+        """Peeks the next token. Identical to calling peek(1)."""
         return self.peek(1)
 
     def peek_previous(self) -> Token:
+        """Peeks the previous token. Identical to calling peek(-1)."""
         return self.peek(-1)
 
     def advance(self) -> Token:
+        """Advances by returning the current token (before the time of call), after incrementing the token counter."""
         if not self.end():
             self.current += 1
         return self.peek_previous()
 
     def check_current_s(self, tokentype: TokenType) -> bool:
+        """Checks if the current token has the given token type."""
         return not self.end() and self.peek_current().token_type == tokentype
 
     def check_current(self, *args: TokenType) -> bool:
+        """Checks if the current token has any of the given token type."""
         for tokentype in args:
             if self.check_current_s(tokentype):
                 return True
         return False
 
     def match_current(self, *args: TokenType) -> bool:
+        """Checks if the current token has any of the given token type. Advances on match."""
         for tokentype in args:
             if self.check_current_s(tokentype):
                 self.advance()
@@ -98,6 +105,7 @@ class Parser(ABC):
         return False
 
     def error(self, token: Token, message: str | None = None) -> ParseError:
+        """Raises a ParseError on the given token with optional message."""
         if not message:
             message = f"Error parsing token {token.lexeme}"
         reference = f" --> <filename>:{token.line}:{token.col}"
@@ -114,6 +122,7 @@ class Parser(ABC):
         return ParseError("\n".join([message, reference, detail]))
 
     def require_token(self, tokentype: TokenType, message: str | None = None) -> Token:
+        """Checks for token type match. Advance if match. Raise error otherwise."""
         if self.check_current_s(tokentype):
             return self.advance()
         raise self.error(self.peek_current(), message)
